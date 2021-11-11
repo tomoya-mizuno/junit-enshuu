7ac76cb00a03e356101668869f2ebe713da702e7
mizunotomoya <tomoya.mizuno@rakus-parters.co.jp>
2021-11-11 10:54:39 +0900

    start practice

---
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -0,0 +1,42 @@
+package junit.tutorial.ex01.e01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StringUtilsTest {
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+	
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	@Test
+	@DisplayName("input \"aaa\" return \"aaa\"")
+	void test0101() {
+		assertEquals("aaa", StringUtils.toSnakeCase("aaa"));
+	}
+	
+	@Test
+	@DisplayName("input \"HelloWorld\" return \"hello_world\"")
+	void test0102() {
+		assertEquals("hello_world", StringUtils.toSnakeCase("HelloWorld"));
+	}
+	
+}
